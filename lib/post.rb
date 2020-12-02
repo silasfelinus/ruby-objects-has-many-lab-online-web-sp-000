4f1ba9e0c380db0b3508a9c1f1824955bee54ead
Silas Michael Knight <silasfelinus@gmail.com>
2020-12-02 01:35:40 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,7 +1,20 @@
 class Post
+  attr_accessor :title
+  @@all = []
 
-  def initialize
+  def initialize(title)
+    @title = title
+    @@all << self
 
+  end
+
+  def author=(author)
+    @author = author
+    self.author.posts << self
+  end
+
+  def author
+    @author
   end
 
 end
