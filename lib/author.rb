8c06e6bddb8bff2ba4959334942ff6c321890101
Silas Michael Knight <silasfelinus@gmail.com>
2020-12-02 01:34:37 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,10 +1,36 @@
 class Author
   attr_accessor :name
+  @@authors = []
+
 
   def initialize(name)
     @name = name
+    @posts = []
+    @@authors << self
 
   end
 
+  def posts
+    @posts
+  end
 
-end
+  def add_post(post)
+      post.author = self
+    end
+
+    def add_post_by_title(title)
+      new_post = Post.new(title)
+      new_post.author = self
+    end
+
+    def self.post_count
+      count = 0
+      @@authors.each do |author|
+        author.post.each do |post|
+          count += 1
+        end
+      end
+      count
+    end
+
+  end
